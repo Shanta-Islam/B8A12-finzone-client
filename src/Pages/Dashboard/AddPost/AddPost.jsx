c660751b3f1497364ca8004c00ad18a299f084ef
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-11-30 21:34:32 +0600

    lastt

--- src/Pages/Dashboard/AddPost/AddPost.jsx
+++ src/Pages/Dashboard/AddPost/AddPost.jsx
@@ -3,10 +3,13 @@ import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Context/AuthProvider";
 import { Box, Button, Grid, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 const AddPost = () => {
     const [tags, setTags] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [users, setUsers] = useState([]);
 
     const axiosPublic = useAxiosPublic();
     const { user } = useContext(AuthContext);
@@ -18,7 +21,19 @@ const AddPost = () => {
                 setTags(res.data)
             })
     }, [axiosPublic])
-    console.log(tags)
+    useEffect(() => {
+        axiosPublic.get(`/postsCount/${user?.email}`)
+            .then(res => {
+                setPosts(res.data)
+            })
+    }, [axiosPublic, user?.email])
+    useEffect(() => {
+        axiosPublic.get(`/user/${user?.email}`)
+            .then(res => {
+                setUsers(res.data)
+            })
+    }, [axiosPublic, user?.email])
+    // console.log(tags)
     const onSubmit = async (data) => {
         const postItems = {
             upVote: 0,
@@ -31,10 +46,101 @@ const AddPost = () => {
         reset()
         console.log(res.data)
     }
+    // console.log(users.status)
     return (
         <Grid elevation={6} square>
-            <Box
-                sx={{
+            {(users?.status || posts.count>5) ?  <Box
+                    sx={{
+                        my: 3,
+                        textAlign: "center",
+                        p: 2,
+                        "& h4": {
+                            fontWeight: "bold",
+                            my: 2,
+                            fontSize: "2rem",
+                        },
+                        "& p": {
+                            textAlign: "justify",
+                        },
+                        "@media (max-width:600px)": {
+                            mt: 0,
+                            "& h4 ": {
+                                fontSize: "1.5rem",
+                            },
+                        },
+                    }}
+                >
+                    <Typography variant="h4">Add Post</Typography>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <TextField
+                            label="Author Image"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            type='text'
+                            {...register("authorImg")}
+                            required
+                        />
+                        <TextField
+                            label="Author Name"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            type='text'
+                            {...register("authorName")}
+                            required
+                        />
+                        <TextField
+                            label="Author Email"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            type='email'
+                            {...register("authorEmail")}
+                            required
+                        />
+                        <TextField
+                            label="Post Title"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            type='text'
+                            {...register("postTitle")}
+                            required
+                        />
+                        <TextField
+                            label="Post Description"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            type='text'
+                            {...register("postDesc")}
+                            required
+                        />
+                        <Select
+                            label="tag"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            {...register("tag")}
+                            required
+                        >
+                            {
+                                tags?.map(tag => <MenuItem key={tag._id} value={tag.data.tag}>{tag.data.tag}</MenuItem>)
+                            }
+                        </Select>
+                        <Button
+                            variant="contained"
+                            sx={{ backgroundColor: '#06BD95', marginTop: '10px' }}
+                            type="submit"
+                            fullWidth
+                        >
+                            Add Post
+                        </Button>
+                    </form>
+
+                </Box > :
+                <Box sx={{
                     my: 3,
                     textAlign: "center",
                     p: 2,
@@ -52,78 +158,12 @@ const AddPost = () => {
                             fontSize: "1.5rem",
                         },
                     },
-                }}
-            >
-                <Typography variant="h4">Add Post</Typography>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <TextField
-                        label="Author Image"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        type='text'
-                        {...register("authorImg")}
-                        required
-                    />
-                    <TextField
-                        label="Author Name"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        type='text'
-                        {...register("authorName")}
-                        required
-                    />
-                    <TextField
-                        label="Author Email"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        type='email'
-                        {...register("authorEmail")}
-                        required
-                    />
-                    <TextField
-                        label="Post Title"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        type='text'
-                        {...register("postTitle")}
-                        required
-                    />
-                    <TextField
-                        label="Post Description"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        type='text'
-                        {...register("postDesc")}
-                        required
-                    />
-                    <Select
-                        label="tag"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        {...register("tag")}
-                        required
-                    >
-                        {
-                            tags?.map(tag => <MenuItem key={tag._id} value={tag.data.tag}>{tag.data.tag}</MenuItem>)
-                        }
-                    </Select>
-                    <Button
-                        variant="contained"
-                        sx={{ backgroundColor: '#06BD95', marginTop: '10px' }}
-                        type="submit"
-                        fullWidth
-                    >
-                        Add Post
-                    </Button>
-                </form>
+                }}>
+                    <Link to='/membership'><Button  variant='contained' sx={{ backgroundColor: '#06BD95', marginTop: '10px' }}>Become a member</Button></Link>
 
-            </Box>
+                </Box>
+               
+            }
         </Grid>
     );
 };
